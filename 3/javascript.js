93d721a1a6aa62c166bd3b8a6eec699b55cf4929
Thomas Barreto <thomi.b137@gmail.com>
2021-07-08 17:09:41 -0300

    3

---
+++ 3/javascript.js
@@ -0,0 +1,18 @@
+const primerNumero = 5;
+const segundoNumero = 4;
+let resultado;
+
+resultado = primerNumero + segundoNumero;
+console.log(primerNumero,"+",segundoNumero,"=", resultado); 
+
+resultado = primerNumero +1;
+console.log(primerNumero,"+ 1 =", resultado); 
+
+resultado = resultado + 6;
+console.log(6,"+ 6 =", resultado); 
+
+resultado = primerNumero / segundoNumero;
+console.log(primerNumero,"/",segundoNumero,"=", resultado);
+
+resultado = primerNumero % segundoNumero;
+console.log(primerNumero,"/",segundoNumero,"=", resultado);
