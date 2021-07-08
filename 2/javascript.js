099970b5221b0159f83d56d47359227a7363c4aa
Thomas Barreto <thomi.b137@gmail.com>
2021-07-08 16:59:08 -0300

    2

---
+++ 2/javascript.js
@@ -0,0 +1,11 @@
+let unaFuncion = new Function ("HolaMundo");
+let unNumero = 1;
+let unaPalabra = "Hola mundo";
+let unaFecha = new Date();
+let unDato;
+
+console.log (typeof unaFuncion);
+console.log (typeof unNumero);
+console.log (typeof unaPalabra)
+console.log (typeof unaFecha)
+console.log (typeof unDato)
