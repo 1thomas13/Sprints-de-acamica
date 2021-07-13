01e6239d64e33f42bc5a8fdd2ab0c0d460e5cda0
Thomas Barreto <thomi.b137@gmail.com>
2021-07-13 14:29:50 -0300

    array-ciclos iterativos

---
+++ 5/javascript.js
@@ -0,0 +1,12 @@
+
+let autos=["Fiesta ","Golf ","Palio ","Focus"];
+
+let i=0;
+while(i< autos.length){
+    console.log(autos[i]);
+    i++;
+}
+
+for(let i=0;i<autos.length;i++){
+    console.log(autos[i]);
+}
