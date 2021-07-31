f3f6c8228c662f8ff1008514b39c5c4354cb1e93
Thomas Barreto <thomi.b137@gmail.com>
2021-07-31 12:17:36 -0300

    challenge

---
+++ clase 10/index.js
@@ -0,0 +1,8 @@
+const chalk=require("chalk")
+
+const saludar="hola"
+
+console.log(chalk.blue(saludar))
+console.log(chalk.red(saludar))
+console.log(chalk.green(saludar))
+console.log(chalk.white(saludar))
