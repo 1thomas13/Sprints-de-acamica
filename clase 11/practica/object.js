ee3061d98c332247f2c1d6482f41ac3470d57317
Thomas Barreto <thomi.b137@gmail.com>
2021-08-06 15:39:29 -0300

    express and postman

---
+++ clase 11/practica/object.js
@@ -0,0 +1,36 @@
+const  phones= [
+    {
+        marca : "samsung",
+        precio: 300,
+        gama: "alta",
+    },
+    {
+        marca : "philips",
+        precio: 200,
+        gama: "media",
+    },
+    {
+        marca : "lg",
+        precio: 300,
+        gama: "baja",
+    },
+    {
+        marca : "huawei",
+        precio: 100,
+        gama: "baja",
+    },
+    {
+        marca : "logitech",
+        precio: 400,
+        gama: "alta",
+    },
+    {
+        marca : "Apple",
+        precio: 200,
+        gama: "Media",
+    }
+]
+
+module.exports={
+    phones
+}
