f6318ed6a39381c573f8750c3289bbc1b9cbf75f
Thomas Barreto <thomi.b137@gmail.com>
2021-09-08 16:52:26 -0300

    temp cities

--- sprint-2/clase 2/practica/index.js
+++ sprint-2/clase 2/practica/index.js
@@ -4,7 +4,7 @@ const axios = require("axios")
 app.use(express.json());
 
 const cities = [
-    {name:"Rio grande"},
+    {name:"Havana"},
     {name:"Singapur"},
     {name:"Macao"},
     {name:"New York"},
@@ -15,43 +15,40 @@ const cities = [
     {name:"Toronto"},
     {name:"Lima"}]
 
-const newCitys = []
+const newCities = []
 
-const randomCity = (newCities,cities) => {
+const randomArrayCity = () => {
 
-    random =  Math.ceil(Math.random()*cities.length-1)
+    random =  Math.floor(Math.random()*cities.length-1)
     newCities.push(cities[random])
 }
 
-async function temps (newCities) {
+const getCityTemp = async (city) => {
+
+    const baseApi = 'http://api.openweathermap.org'
+
+    const res = await axios.get(`${baseApi}/data/2.5/weather?q=${city}&appid=87bae2ffee43eec860796931e362d3ad&units=metric`)
+    return res.data.main.temp;
+}
+
+async function climaController(req,res) {
+   
+
+    while(newCities.length < 3){
+        randomArrayCity()
+    }
+
     try {
-            const getData = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${newCities.name}&appid=87bae2ffee43eec860796931e362d3ad&units=metric`)
-            const temp = getData.data.main.temp;
-            console.log(temp);
+        for await (let city of newCities) {
+            city.temp = await getCityTemp(city.name);
         }
-    catch (error) {
-        throw Error(error);
+        res.status(200).json({Citys:newCities})
+
+    } catch (err) {
+        res.status(500).json({error:err})
     }
 }
 
-app.get("/citys",async(req,res) => {
-
-    let ran = randomCity(cities, 3);
-    const finalList = [];
-    for (city of ran){
-        try {
-            const temp = await tempsCity(name);
-            const item = { name, temp };
-            console.log(`Datos recibidos:`, item);
-            finalList.push(item)
-        } catch (error) {
-            res.status(500).json({ msg: "Error al obtener los datos del clima" });
-            return
-        }
-    }
-
-
-    res.status(200).json({Citys:newCitys})
-})
+app.get("/citys",climaController)
 
 app.listen("3000",()=>{console.log("Escuchando el puero 3000")})
