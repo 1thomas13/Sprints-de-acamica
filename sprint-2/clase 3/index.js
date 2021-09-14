0629973dba2d4630637460f7613eacb841e95aec
Thomas Barreto <thomi.b137@gmail.com>
2021-09-14 13:28:25 -0300

    mongoDB

---
+++ sprint-2/clase 3/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+app.use(express())
+mongoose.connect("mongodb://localhost:27017/bd")
+
+
+schema = {nombre: String, apellido: String, edad: Number}
+const usuarios = mongoose.model("usuarios", schema);
+
+const yo = {nombre: "Juan",
+            apellido: "Perez",
+            edad: 24};
+
+    let nuevo_usuario = new usuarios(yo)
+    nuevo_usuario.save();
+
+usuarios.find().then(function (resultados){
+    console.log(resultados);
+});
+
+usuarios.find({nombre:"Juan"}).then(function (resultados){
+    console.log(resultados);
+    });
+
+app.listen(3000,()=>{
+    console.log("Puerto 27017 corriendo")
+})
